512443bdecbd035a90f0ca6ee13995bce48d89a6
emil shiju <emilshiju10@gmail.com>
2025-07-08 18:59:01 +0530

    fixed all

--- src/lib/axios_client.ts
+++ src/lib/axios_client.ts
@@ -2,7 +2,8 @@
 import axios from 'axios';
 
 const axiosClient = axios.create({
-  baseURL: 'http://localhost:3000/api',
+  // baseURL: 'http://localhost:3000/api',
+  baseURL: 'https://props-24.vercel.app/api',
 });
 
 axiosClient.interceptors.request.use(
--- src/lib/axios_server.ts
+++ src/lib/axios_server.ts
@@ -1,4 +1,5 @@
-const  baseURL ='http://localhost:3000/api/admin'
+// const  baseURL ='http://localhost:3000/api/admin'
+const  baseURL ='https://props-24.vercel.app/api/admin'
 
 const fetchServer = baseURL
 
